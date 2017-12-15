3b06ae726302dfcf384c1eec5ac6f2b71ae7ff53
sukeshsangam <ssang001@odu.edu>
2017-12-15 17:26:11 -0500

    Commit

---
+++ README.txt
@@ -0,0 +1,25 @@
+Name: Sukesh Sangam
+UIN: 01060894
+
+
+Instructions : 
+
+Requires python 2.7
+
+execution python <python file> < optdigits-orig.windep
+
+Used the 80 % of data as testing and 20 % of data as training
+
+The testing and training data files are included in the folder.
+
+
+The output for 10,100,500 hidden layears screenshorts are included in this folder..
+
+We can observe that as we increase the no. of hidden layes the accuracy inceases.
+
+
+Using 1024 features for neural network is very cumbersome
+To avoid this we can take the bits which only covers the number in square shape. and can consider as the features.
+Through this we can reduce the no. of features and works effectively.
+
+
---
+++ neural_500.py
@@ -0,0 +1,45 @@
+
+from sklearn.neural_network import MLPClassifier
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import classification_report
+import numpy as np
+from numpy import array
+digits=[]
+values=[]
+i=0
+digits.append([])
+while 1:
+	row=raw_input()
+	if row=='lastparag = 0':
+		break
+while 1:
+	row=raw_input()
+	if row=="EOF":
+		break
+	else:	
+		if i<32:
+			for c in list(row):
+				digits[-1].append(int(c))
+			i+=1
+		if i==32:
+			number=raw_input()
+			values.append(number.strip(' \t\n\r'))
+			i=0
+			digits.append([])
+digits.pop()
+
+clf = MLPClassifier(solver='lbfgs', alpha=1e-5,hidden_layer_sizes=(500))
+X_train, X_test, y_train, y_test = train_test_split(digits, values, test_size=0.2)
+
+np.savetxt("training_x.txt",array(X_train))
+
+np.savetxt("target_x.txt",array(X_test))
+
+
+#print X_train
+
+model = clf.fit(X_train, y_train)
+predictions = clf.predict(X_test)
+target_names = ['0','1','2','3','4','5','6','7','8','9']
+print classification_report(y_test, predictions, target_names=target_names)
+
